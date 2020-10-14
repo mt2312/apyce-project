9c18f130314bf1bbcb6069b5276bc372d605e671
Mateus Tosta <metzkerrr@gmail.com>
2020-10-14 12:12:50 -0300

    git ignore

--- teste.py
+++
@@ -1,8 +0,0 @@
-from APyCE import *
-
-G = Apyce('./Data/dome.grdecl')
-#G = Apyce('./Data/Norne.GRDECL')
-#G = Apyce('./Data/Johansen.grdecl')
-#G = Apyce("./Data/MAC7A-TM02.txt")
-G.processGRDECL()
-G.WriteVTK()
